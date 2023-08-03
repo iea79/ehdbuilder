4205181467ff5c0f16df5e8e36bf345bf355c186
Evgeniy Ivanov <busforward@gmail.com>
2023-08-03 16:54:59 +0500

    Update

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -1,17 +1,12 @@
 <div class="loader">
     <div class="loader__top">From Vision <br>to Reality</div>
-    <!-- <div class="loader__top">Building <br>homes and trust </div> -->
     <div class="loader__el"></div>
-    <!-- <div class="loader__img">
-        <img src="<?php echo get_template_directory_uri() . '/img/loader.png' ?>" alt="loading">
-    </div> -->
     <div class="loader__percent">0%</div>
     <script>
         let loaderInterval,
             loaderPercent = 0;
         const percentWrap = document.querySelector('.loader__percent');
 
-        // function showLoader() {
         loaderInterval = setInterval(() => {
             if (loaderPercent <= 100) {
                 percentWrap.innerHTML = loaderPercent + '%';
@@ -20,7 +15,7 @@
                 loaderPercent = 0;
             }
         }, 50);
-        // }
+        window.onload = () => document.querySelector('.loader').style.display = "none";
     </script>
 </div>
 <div class="homePage">
